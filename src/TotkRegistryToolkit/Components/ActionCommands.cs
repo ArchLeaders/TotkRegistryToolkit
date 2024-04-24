d22c716dd67a0f34a25d63fd8c8ac65c2fb27d29
ArchLeaders <archleadership28@gmail.com>
2024-04-24 11:06:45 -0700

    Impl app and action commands

---
+++ src/TotkRegistryToolkit/Components/ActionCommands.cs
@@ -0,0 +1,13 @@
+﻿using Cocona;
+using TotkRegistryToolkit.Services;
+
+namespace TotkRegistryToolkit.Components;
+
+public class ActionCommands
+{
+    [Command("run", Description = "Run a feature command (used internally)")]
+    public static async Task Run([Argument] string feature, [Argument] string[] args)
+    {
+        await FeatureService.Execute(feature, args);
+    }
+}
--- src/TotkRegistryToolkit/Program.cs
+++ src/TotkRegistryToolkit/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Cocona;
+using System.Reflection;
+using TotkRegistryToolkit.Components;
+
+Console.WriteLine($"""
+    TotK Registry Toolkit [Version {Assembly.GetExecutingAssembly().GetName().Version?.ToString(3) ?? "???"}]
+    (c) 2024 ArchLeaders. MIT.
+
+    """);
+
+CoconaLiteApp app = CoconaLiteApp.Create(args);
+app.AddCommands<ActionCommands>();
+await app.RunAsync();

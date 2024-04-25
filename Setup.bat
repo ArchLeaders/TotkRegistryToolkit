0fe637aac4acfdbe7ae9163180672451b5162c8f
ArchLeaders <archleadership28@gmail.com>
2024-04-24 21:40:00 -0700

    Create setup script; update release workflow

---
+++ Setup.bat
@@ -0,0 +1,2 @@
+curl -o "%LOCALAPPDATA%\TotkRegistryToolkit\tkrt.exe" "https://github.com/ArchLeaders/TotkRegistryToolkit/releases/latest/download/win-x64.exe"
+"%LOCALAPPDATA%\TotkRegistryToolkit\tkrt.exe" init

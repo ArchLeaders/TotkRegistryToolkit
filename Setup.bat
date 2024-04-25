57e9122eb3148a58a49008755dda6e0a923306d8
ArchLeaders <archleadership28@gmail.com>
2024-04-24 23:16:10 -0700

    Fix setup script (again)

--- Setup.bat
+++ Setup.bat
@@ -1,3 +1,5 @@
+@ECHO OFF
+
 curl --silent -o "%LOCALAPPDATA%\TotkRegistryToolkit\tkrt.exe" -L "https://github.com/ArchLeaders/TotkRegistryToolkit/releases/latest/download/win-x64.exe"
 "%LOCALAPPDATA%\TotkRegistryToolkit\tkrt.exe" init
 

3bffceb9d1830e75b99988f665f32102dc832e27
ArchLeaders <archleadership28@gmail.com>
2024-04-25 22:10:55 -0700

    Update dependencies

--- src/TotkRegistryToolkit/Features/ZsFeature.cs
+++ src/TotkRegistryToolkit/Features/ZsFeature.cs
@@ -1,8 +1,7 @@
 ﻿using CommunityToolkit.HighPerformance.Buffers;
-using Totk.Common.Extensions;
+using TotkCommon;
 using TotkRegistryToolkit.Attributes;
 using TotkRegistryToolkit.Extensions;
-using TotkRegistryToolkit.Models;
 using TotkRegistryToolkit.Services;
 
 namespace TotkRegistryToolkit.Features;
@@ -12,8 +11,6 @@ public class ZsFeature : IFeature
 {
     public static async ValueTask Execute(string[] args)
     {
-        ZstdExtension.LoadDictionaries(TotkConfig.Shared.ZsDicPath);
-
         await WorkloadService.DelegateTasks(args, Process, CancellationToken.None);
     }
 
@@ -25,7 +22,7 @@ public class ZsFeature : IFeature
         Span<byte> data = buffer.Span;
         fs.Read(data);
 
-        return data.IsZsCompressed() switch {
+        return Zstd.IsCompressed(data) switch {
             true => Decompress(data, path),
             false => Compress(data, path),
         };
@@ -33,8 +30,8 @@ public class ZsFeature : IFeature
 
     private static ValueTask Decompress(Span<byte> data, string path)
     {
-        using SpanOwner<byte> decompressed = SpanOwner<byte>.Allocate(data.GetZsDecompressedSize());
-        data.ZsDecompress(decompressed.Span, out int id);
+        using SpanOwner<byte> decompressed = SpanOwner<byte>.Allocate(Zstd.GetDecompressedSize(data));
+        Totk.Zstd.Decompress(data, decompressed.Span, out int id);
 
         using FileStream fs = File.Create(path.TrimExtension());
         fs.Write(decompressed.Span);
@@ -48,7 +45,7 @@ public class ZsFeature : IFeature
         ReadOnlySpan<char> subExt = Path.GetExtension(Path.GetFileNameWithoutExtension(path.AsSpan()));
 
         using SpanOwner<byte> compressed = SpanOwner<byte>.Allocate(data.Length);
-        int size = data.ZsCompress(compressed.Span, ext switch {
+        int size = Totk.Zstd.Compress(data, compressed.Span, ext switch {
             ".pack" => 3,
             ".byml" => subExt switch {
                 ".bcett" => 2,
